fa2f4425a38c61cb90a0e1fa9dd90f053d061f2f
Aaron Didner <adidner@gmail.com>
2022-01-09 15:36:21 -0500

    can save our data to chrome storage

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
         console.log("add new row");
         let numberRows = document.getElementById(targetContentColumnId).childElementCount - 2;
         let newNumberRows = numberRows + 1;
-        let newButtonRowId = targetContentColumnId + newNumberRows;
+        let newButtonRowId =  newNumberRows + "-" + targetContentColumnId;
 
         var row = document.createElement("div");
         row.setAttribute("class", "row");
@@ -27,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
         input.setAttribute("type", "text");
         input.setAttribute("class", "title-input");
         input.setAttribute("placeholder", "set speed for");
+        input.setAttribute("id", "input-" + newButtonRowId);
         row.appendChild(input);
 
         var select = document.createElement("select");
@@ -51,11 +52,12 @@ document.addEventListener("DOMContentLoaded", function () {
     
     
     function AddContentColumn(){
-        let numberRows = document.body.childElementCount - 1;
-        let newContentColumnId = numberRows + 1;
+        let contentColumns = document.body.childElementCount - 1;
+        let nextId = (contentColumns + 1);
+        let newContentColumnId = "content-column-" + nextId;
 
         var contentColumn = document.createElement("div");
-        contentColumn.setAttribute("id", "content-column-" + newContentColumnId);
+        contentColumn.setAttribute("id", newContentColumnId);
         contentColumn.setAttribute("class", "content-column");
 
         var speedRow = document.createElement("div");
@@ -67,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
         
         var speedInput = document.createElement("input");
         speedInput.setAttribute("type", "number");
+        speedInput.setAttribute("id", "speed-input-" + newContentColumnId); 
         speedRow.appendChild(speedInput);
         
         contentColumn.appendChild(speedRow);
@@ -78,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
         var addNewRowButtonButton = document.createElement("div");
         addNewRowButtonButton.setAttribute("class","appear-button-like")
         addNewRowButtonButton.innerText = "Add New Row";
-        addNewRowButtonButton.addEventListener("click", () => AddNewRowButton("content-column-" + newContentColumnId));
+        addNewRowButtonButton.addEventListener("click", () => AddNewRowButton(newContentColumnId));
         
         addNewRowButtonContainer.appendChild(addNewRowButtonButton);
 
@@ -90,13 +93,53 @@ document.addEventListener("DOMContentLoaded", function () {
             body.children[body.childElementCount]
         );
 
-        AddNewRowButton("content-column-" + newContentColumnId);
+        AddNewRowButton(newContentColumnId);
         
     }
-        
+
+    function saveAndApplyChanges(){
+        console.log("save and apply");
+        let saveObject = [
+
+        ];
+        let numberContentSection = document.body.childElementCount - 1;
+        for(let i = 1; i < numberContentSection + 1; i++){    
+            contentSectionId = "content-column-" + i;
+            contentSection = document.getElementById(contentSectionId);
+            let numberRows = contentSection.childElementCount - 2;
+            console.log("numberRows", numberRows);
+            let contentSectionObject = {
+                speed: document.getElementById("speed-input-" + "content-column-" + i).value,
+                rows: [
+
+                ],
+            }
+            for(let j = 1; j < numberRows + 1; j++){
+                let targetInput = document.getElementById("input-"+ j + "-content-column-" + i);
+                let rowObject = {
+                    inputValue: targetInput.value,
+                    selectValue: "channel",
+                }
+                contentSectionObject.rows.push(rowObject);
+            }
+            saveObject.push(contentSectionObject);
+        }
+        console.log("saveObject", saveObject);
+        chrome.storage.sync.set({saveObject: saveObject})
+    }
+
+    function rehydrateSavedData(){
+        //if we have no saved data, make one column
+        // if(){
+            AddContentColumn();
+        // }
+        // else grab the saved data and populate the structure with it
+    } 
+
     // document.querySelector("#add-new-row-button-1").addEventListener("click", AddNewRowButton);
     // document.querySelector("#delete-button-1").addEventListener("click", () => DeleteRowButton("row-1"));
     document.querySelector('#add-content-column').addEventListener("click", () => AddContentColumn());    
-    AddContentColumn();
+    document.querySelector('#save-button').addEventListener("click", () => saveAndApplyChanges());
+    rehydrateSavedData();
 });
   
