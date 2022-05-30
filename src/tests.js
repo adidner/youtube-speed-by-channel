200d861296b3913e7767fc1891e9de36c43aa8fe
Aaron Didner <adidner@gmail.com>
2022-05-30 18:54:59 -0400

    test outlines

---
+++ src/tests.js
@@ -0,0 +1,41 @@
+
+
+// I want to implement tests later (did some prelimiary research and it seems difficult for chrome extensions)
+
+
+
+it('Adds New Speed Sections and persists', () => {
+
+});
+
+it('Deletes Speed Sections and persists', () => {
+
+});
+
+it('Adds New Rows and persists', () => {
+
+});
+
+it('Deletes Rows and persists', () => {
+
+});
+
+it('changes between title and channel and persists', () => {
+
+});
+
+it('Sets a matching input and persist', () => {
+
+});
+
+it('Sets speed in a youtube video correctly', () => {
+
+});
+
+it('Does not set speed when the video is not white listed', () => {
+
+});
+
+it('Matches content correctly', () => {
+
+});
